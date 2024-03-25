64369c1f72ab014270a18c832ce0be2dab8dcae6
Milo <mil.swiatek1@gmail.com>
2024-03-25 07:04:02 +0000

    string calculator done

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -7,9 +7,22 @@ class StringCalculator
 
     public function add(string $numbers): int
     {
+        $addends = $this->parseInput($numbers);
 
-        return 0;
-
+        return array_sum($addends);
     }
 
+    private function parseInput(string $numbers): array
+    {
+
+        if (!preg_match('/^(\d+((,|\\\n)\d+)*)?$/', $numbers)) {
+            throw new \UnexpectedValueException('Invalid Input');
+        }
+
+        $normalizedInput = preg_replace('/\\\n/', ',', $numbers);
+        $addends= explode(',', $normalizedInput);
+
+        return array_map(fn ($item) => (int) $item, $addends);
+
+    }
 }
--- tests/StringCalculatorKataTest.php
+++ tests/StringCalculatorKataTest.php
@@ -2,27 +2,125 @@
 
 namespace Test;
 
-use PHPUnit\Framework\TestCase;
 use App\StringCalculator;
+use http\Exception\UnexpectedValueException;
+use PHPUnit\Framework\TestCase;
 
 class StringCalculatorKataTest extends TestCase
 {
+    /**
+     * @dataProvider dataProviderForTestAdd
+     */
+    public function testAdd(string $numbers, int $expected)
+    {
+        //given
+        $calculator = new StringCalculator();
+        // when
+        $actual = $calculator->add($numbers);
+        //then
+        $this -> assertSame($expected, $actual);
+    }
 
-    public function testAdd()
+    public function dataProviderForTestAdd(): \Generator
+    {
+        yield 'should return 0 for 0' => [
+            'numbers' => '0',
+            'expected' => 0
+        ];
+        yield 'should return 0 for ""' => [
+            'numbers' => '""',
+            'expected' => 0
+        ];
+        yield 'should return 2 for 2' => [
+            'numbers' => '2',
+            'expected' => 2
+        ];
+        yield 'should return 3 for `1,2`' => [
+            'numbers' => '1,2',
+            'expected' => 3
+        ];
+        yield 'should return 10 for `3,7`' => [
+            'numbers' => '3,7',
+            'expected' => 10
+        ];
+        yield 'should return 21 for `3,7,11`' => [
+            'numbers' => '3,7,11',
+            'expected' => 21
+        ];
+        yield 'should return 6 for `1,2,3`' => [
+            'numbers' => '1,2,3',
+            'expected' => 6
+        ];
+        yield 'should return 10 for `1,2,3,4`' => [
+            'numbers' => '1,2,3,4',
+            'expected' => 10
+        ];
+        yield 'should return 3 for `1\n2`' => [
+            'numbers' => '1\n2',
+            'expected' => 3
+        ];
+        yield 'should return 6 for `1\n2\n3`' => [
+            'numbers' => '1\n2\n3',
+            'expected' => 6
+        ];
+        yield 'should return 6 for `1\n2,3`' => [
+            'numbers' => '1\n2,3',
+            'expected' => 6
+        ];
+        yield 'should return 6 for `1,2\n3`' => [
+            'numbers' => '1,2\n3',
+            'expected' => 6
+        ];
+
+    }
+    /**
+     * @dataProvider dataProviderForTestAddWithException
+     */
+    public function AddWithException(string $numbers, string $expectedException)
     {
 
         //given
-
         $calculator = new StringCalculator();
-        $expected = 0;
-
         //when
+        $actual = $calculator->add($numbers);
+        //then
+        $this->expectException($expectedException);
+    }
 
-        $actual = $calculator->add("");
+    public function dataProviderForTestAddWithException(): \Generator
+    {
 
-        //add
-
-        $this->assertSame($expected, $actual);
-
+        yield 'should throw UnexpectedValueException for `1,\n`' => [
+            'numbers' => '1,\n',
+            'expectedException' => \UnexpectedValueException::class
+        ];
+        yield 'should throw UnexpectedValueException for `1\n,`' => [
+            'numbers' => '1\n,',
+            'expectedException' => \UnexpectedValueException::class
+        ];
+        yield 'should throw UnexpectedValueException for `1\n\n`' => [
+            'numbers' => '1\n\n',
+            'expectedException' => \UnexpectedValueException::class
+        ];
+        yield 'should throw UnexpectedValueException for `1,,`' => [
+            'numbers' => '1,,',
+            'expectedException' => \UnexpectedValueException::class
+        ];
+        yield 'should throw UnexpectedValueException for `1,,2`' => [
+            'numbers' => '1,,2',
+            'expectedException' => \UnexpectedValueException::class
+        ];
+        yield 'should throw UnexpectedValueException for `1\n\n2`' => [
+            'numbers' => '1\n\n2',
+            'expectedException' => \UnexpectedValueException::class
+        ];
+        yield 'should throw UnexpectedValueException for `,,1`' => [
+            'numbers' => ',,1',
+            'expectedException' => \UnexpectedValueException::class
+        ];
+        yield 'should throw UnexpectedValueException for `\n\n1`' => [
+            'numbers' => '\n\n1',
+            'expectedException' => \UnexpectedValueException::class
+        ];
     }
 }
